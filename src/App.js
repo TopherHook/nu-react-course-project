f4f4830276f95104858973c5c8d47feb3330415f
Topher Hook <topherhook@gmail.com>
2023-05-17 15:40:23 -0700

    Async Campsites

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
@@ -6,8 +8,15 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 import AboutPage from './pages/AboutPage';
 import { Header, Footer } from './components';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCampsites());
+    }, [dispatch]);
+
     return (
         <div className='App'>
             <Header />
